9602ba8135fe17e0c2520a8424fa7ba6391c33ab
Sancha-aesthetic~ <joseantonio.sanchachausal@educa.madrid.org>
2025-10-13 13:04:55 -0500

    V1

---
+++ src/main/java/es/etg/dam/grep/Lanzador/Lanzador.java
@@ -0,0 +1,29 @@
+package es.etg.dam.grep.Lanzador;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class Lanzador {
+
+    public static final String TEXTO = "Me gusta PSP y java PSP se programa en java es un módulo de DAM y se programa de forma concurrente en PSP PSP es programación.";
+    public static final String TEXT = "Linea psp";
+    public static void main(String[] args) throws Exception {
+        Process p = Runtime.getRuntime().exec("grep PSP");
+        OutputStream out = p.getOutputStream();
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+        pw.println(TEXTO);
+        pw.close();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        System.out.println(TEXT);
+
+        String linea;
+        while ((linea = br.readLine()) != null) {
+            System.out.println(linea);
+        }
+        br.close();
+    }
+}
